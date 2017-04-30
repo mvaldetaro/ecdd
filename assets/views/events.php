042ea478a2ea33dc2bc2d2b2206c1edd225cd64c
Magno Valdetaro <mvaldetaro@gmail.com>
2017-04-30 09:10:31 -0300

    Implementa bloco eventos

--- assets/views/events.php
+++ assets/views/events.php
@@ -1,15 +1,31 @@
 <section class="events">
   <header class="events__header">
     <h2 class="title">Eventos</h2>
-    <a class="go-to-link" href="#" title="">Agenda Completa</a>
+    <a class="go-to-link" href="<?php bloginfo('url'); ?>/category/eventos" title="Eventos passados">Outros eventos</a>
   </header>
   <div class="events__content">
-    <figure class="events__content__figure">
-      <img src="" alt="evento em destaque">
-    </figure>
-    <figcaption class="events__content__event__caption">
-      <h3>Expo ECDD</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus, veritatis!</p>
-    </figcaption>
-  </div>
+
+    <?php
+    $args = array(
+      'category_name' => 'eventos',
+      'post_status' => 'publish',
+      'posts_per_page' => 1
+      );
+    $query = new WP_Query($args);
+    if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post();
+    ?>
+      <figure class="events__content__figure">
+        <?php the_post_thumbnail('large'); ?>
+      </figure>
+      <figcaption class="events__content__caption">
+        <h3><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
+        <p><?php the_excerpt(); ?></p>
+      </figcaption>
+    <?php
+    endwhile;
+    wp_reset_postdata();
+    else : ?>
+    <p><?php _e('Nada encontrado.'); ?></p>
+  <?php endif; ?>
+</div>
 </section>
